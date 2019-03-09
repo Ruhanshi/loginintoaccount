af3955dddd0636eef1eb247df189694470feaffc
Ruhanshi <48256693+Ruhanshi@users.noreply.github.com>
2019-03-09 14:22:26 +0530

    Create process.php

---
+++ process.php
@@ -0,0 +1,21 @@
+<?php 
+// Get values passe from form in login.php file
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $username = stripcslashes($username);
+  $password = stripcslashes($password);
+  $username = mysql_real_escape_string($username);
+  $password = mysql_real_escape_string($password);
+  
+  mysql_connect("localhost", "root", "");
+  mysql_select_db("loginintoaccount");
+  $result = mysql_query("select * from users where username = '$username' and password = '$password'") or die("failed to query database" .mysql_error());
+  $row = mysql_fetch_array($result);
+  if($row['username'] == $username && $row['password'] == $password)
+  	{echo "login success!!!!" .$row['username'];
+}
+  	else
+  { echo "failed to login!";
+}
+?>
