cb21aa382a2c46dea499d89599502ab80dec00b7
Ruhanshi <48256693+Ruhanshi@users.noreply.github.com>
2019-03-09 14:20:46 +0530

    Create loginintoaccount.php

---
+++ loginintoaccount.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login Page</title>
+	<link rel="stylesheet" type="text/css" href="template.css"> </head>
+<body>
+	<div id="frm">
+		<form action="process.php" method="POST">
+			<p>
+				<label>Username:</label>
+				<input type="text" id="username" name="username" />
+				</p>	
+				<p>
+				<label>Password:</label>
+				<input type="password" id="password" name="password" />
+				</p>	
+				
+				<p>
+				<input type="submit" id="btn" value="Login" />
+				</p>		
+		</form>		
+	</div>
+
+</body>
+</html>
